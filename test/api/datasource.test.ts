660a734a58358bd5ed9d0f1eb784c6ee18468ac8
German Poljakov <gerpol17@gmail.com>
2019-10-01 15:10:12 +0200

    fix: post/ test

--- test/api/datasource.test.ts
+++ test/api/datasource.test.ts
@@ -1,48 +1,84 @@
 import request from 'supertest';
 import { expect } from 'chai';
 import app from '../../src/app';
-import yjs from 'js-yaml';
-import fs from 'fs';
-import { path as approot } from 'app-root-path';
-import path from 'path';
 
-const CONTENT_TYPE_JSON = 'application/json';
+import { Utils } from '../../src/utils/utils';
+import { OpenApiValidator } from 'express-openapi-validate';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let spec: any;
-try {
-  spec = yjs.safeLoad(
-    fs.readFileSync(path.join(approot, 'fdk-harvest-adm.yaml'), 'utf-8')
-  );
-} catch (e) {
-  console.log(e);
-}
+const openApiDocument = Utils.readOpenApi();
+const validator = new OpenApiValidator(openApiDocument);
 
-// TODO(chlenix): replace with AJV schema validator
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const validateDatasourceSchema = (datasource: any): void => {
-  const schema = spec.components.schemas.DataSource;
-  expect(datasource).to.be.an(schema.type);
-  expect(datasource).to.have.all.keys(...Object.keys(schema.properties));
-};
+beforeEach(done => {
+  mongoose.Promise = global.Promise;
+  const mongoTestServer = new MongoMemoryServer();
+
+  const options = {
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  };
+
+  mongoTestServer
+    .getConnectionString()
+    .then(uri => {
+      mongoose.connection.once('open', () => {
+        console.log(`MongoDB successfully connected to ${uri}`);
+      });
+      mongoose
+        .connect(uri, options)
+        .then(() => {
+          mongoose.connection.on('error', e => {
+            if (e.message.code === 'ETIMEDOUT') {
+              console.log(e);
+              mongoose.connect(uri, options);
+            }
+            console.log(e);
+          });
+          done();
+        })
+        .catch(done);
+    })
+    .catch(done);
+});
+
+afterEach(done => {
+  mongoose
+    .disconnect()
+    .then(done)
+    .catch(done);
+});
 
 describe('/api/datasources', () => {
-  const supportedMethods = spec.paths['/datasources'];
-  const requestMeta = supportedMethods.get;
+  const supportedMethods = openApiDocument.paths['/datasources'];
 
-  it(requestMeta.description, () => {
-    const responses = requestMeta.responses;
+  const { post } = supportedMethods;
+  it(post.description || post.summary, done => {
+    const { responses } = post;
     const code = Object.keys(responses)[0];
-    request(app)
-      .get(`/api/datasources`)
-      .expect(parseInt(code, 10))
-      .then(res => {
-        expect(res.body).to.be.an(
-          responses[code].content[CONTENT_TYPE_JSON].schema.type
-        );
+    const validateResponse = validator.validateResponse('post', '/datasources');
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        res.body.map(validateDatasourceSchema);
+    const url = 'http://example.com';
+
+    request(app)
+      .post(`/api/datasources`)
+      .send({
+        id: 'fx434358zx45sdss11',
+        dataSourceType: 'SKOS-AP-NO',
+        url: url,
+        publisher: 'hi',
+        description: 'Bla bla bla'
+      })
+      .expect(parseInt(code))
+      .expect('Location', url)
+      .then(res => {
+        expect(validateResponse(res));
+        done();
       })
       .catch(err => expect(err).to.be.undefined);
   });
