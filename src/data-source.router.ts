e410b26909023d7ef910df58478015eb906a0753
Markko Paas <markko.paas@gmail.com>
2019-10-03 21:23:32 +0200

    feat: #2349 add validation using spec

--- src/data-source.router.ts
+++ src/data-source.router.ts
@@ -1,13 +1,30 @@
 import { Router } from 'express';
 import { dataSourceHandlers } from './data-source.handlers';
+import { dataSourceApiValidator } from './data-source.validator';
 
 export const dataSourcesPath = '/datasources';
 export const dataSourceRouter = Router();
 
-dataSourceRouter.get(`${dataSourcesPath}`, dataSourceHandlers.getAll);
+dataSourceRouter.get(
+  `${dataSourcesPath}`,
+  dataSourceApiValidator.validate('get', `${dataSourcesPath}`),
+  dataSourceHandlers.getAll
+);
 
-dataSourceRouter.post(`${dataSourcesPath}`, dataSourceHandlers.create);
+dataSourceRouter.post(
+  `${dataSourcesPath}`,
+  dataSourceApiValidator.validate('post', `${dataSourcesPath}`),
+  dataSourceHandlers.create
+);
 
-dataSourceRouter.get(`${dataSourcesPath}/:id`, dataSourceHandlers.getById);
+dataSourceRouter.get(
+  `${dataSourcesPath}/:id`,
+  dataSourceApiValidator.validate('get', `${dataSourcesPath}/{id}`),
+  dataSourceHandlers.getById
+);
 
-dataSourceRouter.put(`${dataSourcesPath}/:id`, dataSourceHandlers.update);
+dataSourceRouter.put(
+  `${dataSourcesPath}/:id`,
+  dataSourceApiValidator.validate('put', `${dataSourcesPath}/{id}`),
+  dataSourceHandlers.update
+);
