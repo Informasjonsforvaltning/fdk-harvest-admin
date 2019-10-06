50bf939328e3f310486418d61bd634aad1048cd7
Markko Paas <markko.paas@gmail.com>
2019-10-06 22:56:41 +0200

    refactor: Use app-root-path to resolve spec file, because typescript gets compiled into dist.

--- src/data-source.validator.ts
+++ src/data-source.validator.ts
@@ -1,7 +1,7 @@
 import { OpenApiDocument, OpenApiValidator } from 'express-openapi-validate';
 import { readSyncYaml } from './lib/read-sync-yaml';
 
-const specPath = __dirname + '/../spec/fdk-harvest-admin.yaml';
+const specPath = require('app-root-path').resolve('/spec/fdk-harvest-admin.yaml');
 export const spec: OpenApiDocument = readSyncYaml(specPath) as OpenApiDocument;
 
 export const dataSourceApiValidator = new OpenApiValidator(spec);
