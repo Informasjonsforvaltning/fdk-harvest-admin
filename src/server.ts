29e77780632f6976cdcde691e00fa2cfca7c4014
Markko Paas <markko.paas@gmail.com>
2019-10-09 15:19:18 +0200

    deployment: use common default mongo username and password in dev environment

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || config.get('server.port');
 const { host, port, name } = config.get('mongodb');
 
 const connectionUris = `mongodb://${process.env.MONGO_USERNAME || ''}:${process
-  .env.MONGO_PASSWORD || ''}@${host}:${port}/${name}`;
+  .env.MONGO_PASSWORD || ''}@${host}:${port}/${name}?authSource=admin&authMechanism=SCRAM-SHA-1`;
 
 createApp({ connectionUris })
   .then(app => {
