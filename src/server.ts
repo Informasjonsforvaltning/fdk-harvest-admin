9f6dfab1eaacae29865d87d9865f3752b8207278
German Poljakov <gerpol17@gmail.com>
2019-10-10 09:11:19 +0200

    fix(mongo-connection): (temp) console logs of mongo connection

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,13 @@ const PORT = process.env.PORT || config.get('server.port');
 const { host, port, name } = config.get('mongodb');
 
 const connectionUris = `mongodb://${process.env.MONGO_USERNAME || ''}:${process
-  .env.MONGO_PASSWORD || ''}@${host}:${port}/${name}?authSource=admin&authMechanism=SCRAM-SHA-1`;
+  .env.MONGO_PASSWORD ||
+  ''}@${host}:${port}/${name}?authSource=admin&authMechanism=SCRAM-SHA-1`;
+
+console.log(
+  'FORMAT: mongodb://MONGO_USERNAME:MONGO_PASSWORD@HOST:PORT/DATABASE_NAME'
+);
+console.log('ACTUAL: ', connectionUris);
 
 createApp({ connectionUris })
   .then(app => {
