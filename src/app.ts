622360eddb9c1e147be4c9c6a3dea3b2be7a2d84
Markko Paas <markko.paas@gmail.com>
2019-10-11 06:46:42 +0200

    refactor: introduce dependency injection for future rabbitmq service

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { commonErrorHandler } from './lib/common-error-handler';
 
 import { connectDb } from './db/db';
-import { dataSourceRouter } from './data-source.router';
+import { createDataSourceRouter } from './data-source.router';
 
 export async function createApp({
   connectionUris
@@ -14,7 +14,7 @@ export async function createApp({
 
   app.use(bodyParser.json());
 
-  app.use('/api', dataSourceRouter);
+  app.use('/api', createDataSourceRouter());
 
   app.use(commonErrorHandler);
 
--- src/data-source.handlers.ts
+++ src/data-source.handlers.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express';
 import omit from 'lodash/omit';
+
 import { DataSourceDocument, DataSourceModel } from './data-source.model';
 import { NotFoundHttpError } from './lib/http-error';
 import { elseThrow } from './lib/else-throw';
@@ -11,40 +12,42 @@ interface ResourceHandlerMap {
   update: RequestHandler;
 }
 
-export const dataSourceHandlers: ResourceHandlerMap = {
-  create: (req, res, next) => {
-    const data = omit(req.body, 'id');
-    new DataSourceModel(data)
-      .save()
-      .then(datasource => {
-        res
-          .location(datasource.id)
-          .status(201)
-          .send();
-      })
-      .catch(next);
-  },
+export const createDataSourceHandlers = (): ResourceHandlerMap => {
+  return {
+    create: (req, res, next): void => {
+      const data = omit(req.body, 'id');
+      new DataSourceModel(data)
+        .save()
+        .then(doc => {
+          res
+            .location(doc.id)
+            .status(201)
+            .send();
+        })
+        .catch(next);
+    },
 
-  update: (req, res, next) => {
-    const { id } = req.params;
-    const data = omit(req.body, 'id');
-    DataSourceModel.findOneAndUpdate({ id }, data, { new: true })
-      .then(elseThrow<DataSourceDocument>(() => new NotFoundHttpError()))
-      .then(doc => res.status(200).send(doc.toObject()))
-      .catch(next);
-  },
+    update: (req, res, next): void => {
+      const { id } = req.params;
+      const data = omit(req.body, 'id');
+      DataSourceModel.findOneAndUpdate({ id }, data, { new: true })
+        .then(elseThrow<DataSourceDocument>(() => new NotFoundHttpError()))
+        .then(doc => res.status(200).send(doc.toObject()))
+        .catch(next);
+    },
 
-  getById: (req, res, next) => {
-    const { id } = req.params;
-    DataSourceModel.findOne({ id })
-      .then(elseThrow<DataSourceDocument>(() => new NotFoundHttpError()))
-      .then(doc => res.status(200).send(doc.toObject()))
-      .catch(next);
-  },
+    getById: (req, res, next): void => {
+      const { id } = req.params;
+      DataSourceModel.findOne({ id })
+        .then(elseThrow<DataSourceDocument>(() => new NotFoundHttpError()))
+        .then(doc => res.status(200).send(doc.toObject()))
+        .catch(next);
+    },
 
-  getAll: (req, res, next) => {
-    DataSourceModel.find(req.query)
-      .then(docs => res.send(docs.map(doc => doc.toObject())))
-      .catch(next);
-  }
+    getAll: (req, res, next): void => {
+      DataSourceModel.find(req.query)
+        .then(docs => res.send(docs.map(doc => doc.toObject())))
+        .catch(next);
+    }
+  };
 };
