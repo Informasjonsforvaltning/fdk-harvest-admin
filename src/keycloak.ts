3abe71499107767c621ac7c7ce98ae16505745d9
German Poljakov <gerpol17@gmail.com>
2019-10-25 13:29:51 +0200

    fix: #33 enforce authority on POST (harvest, create), PUT and DELETE

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,4 +1,21 @@
-import Keycloak from 'keycloak-connect';
+import Keycloak, { Token } from 'keycloak-connect';
 import config from 'config';
+import { RequestHandler } from 'express';
 
-export default new Keycloak({}, config.get('keycloak'));
+const {
+  config: keycloakConfig,
+  'allowed-authority': allowedAuthority
+} = config.get('keycloak');
+
+const keycloakInstance = new Keycloak({}, keycloakConfig);
+
+const checkAuthority = ({ content }: Token): boolean => {
+  const { authorities } = content as any;
+  return authorities && authorities.split(',').includes(allowedAuthority);
+};
+
+export const enforceAuthority = (): RequestHandler => {
+  return keycloakInstance.protect(checkAuthority);
+};
+
+export default keycloakInstance;
