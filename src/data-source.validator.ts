2012e049e33e9557df10f3a53aa9d9b1cff39e2d
German Poljakov <gerpol17@gmail.com>
2019-12-02 14:19:59 +0100

    fix: #2749 added routing key extensions

--- src/data-source.validator.ts
+++ src/data-source.validator.ts
@@ -1,8 +1,9 @@
 import { OpenApiDocument, OpenApiValidator } from 'express-openapi-validate';
 import { readSyncYaml } from './lib/read-sync-yaml';
+import config from 'config';
 
 const specPath = require('app-root-path').resolve(
-  '/spec/fdk-harvest-admin.yaml'
+  `/spec/${config.get('spec.open-api')}`
 );
 export const spec: OpenApiDocument = readSyncYaml(specPath) as OpenApiDocument;
 
