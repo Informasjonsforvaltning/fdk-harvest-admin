52b17a3737eb3f68c1cde68c7c7997bfc938b23a
German Poljakov <gerpol17@gmail.com>
2019-12-02 14:19:59 +0100

    fix: #2749 added routing key extensions

--- src/rabbitmq/asyncspec-validator.ts
+++ src/rabbitmq/asyncspec-validator.ts
@@ -1,5 +1,11 @@
 import AsyncApiValidator from 'asyncapi-validator';
+import { readSyncYaml } from '../lib/read-sync-yaml';
+import config from 'config';
+
+const asyncSpecFile = require('app-root-path').resolve(
+  `/spec/${config.get('spec.async-api')}`
+);
 
 export const validator = AsyncApiValidator.fromSource(
-  'https://raw.githubusercontent.com/Informasjonsforvaltning/fdk-message-broker/master/fdk-message-broker.yaml'
+  readSyncYaml(asyncSpecFile)
 );
