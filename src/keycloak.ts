14fdff15c8e28bbb3b19fca0c9b952a521e3271c
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-08-13 15:45:25 +0200

    feat: #3150 allow for organization admin role

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,17 +1,38 @@
-import Keycloak, { Token } from 'keycloak-connect';
+import Keycloak, { SpecHandler } from 'keycloak-connect';
 import config from 'config';
 import { RequestHandler } from 'express';
 
-const {
-  config: keycloakConfig,
-  'allowed-authority': allowedAuthority
-} = config.get('keycloak');
+const { config: keycloakConfig } = config.get('keycloak');
 
 const keycloakInstance = new Keycloak({}, keycloakConfig);
 
-const checkAuthority = ({ content }: Token): boolean => {
-  const { authorities } = content as any;
-  return authorities && authorities.split(',').includes(allowedAuthority);
+const checkAuthority: SpecHandler = ({ content }, _, res) => {
+  const { authorities: authoritiesString } = content as any;
+
+  const authorities: string[] = authoritiesString
+    ? authoritiesString.split(',')
+    : [];
+
+  const hasSystemAdminRootPermission = authorities.includes(
+    'system:root:admin'
+  );
+
+  const hasOrganizationAdminPermissions = authorities.some(authority =>
+    authority.match(/^organization:\d{9}:admin$/)
+  );
+
+  if (hasOrganizationAdminPermissions) {
+    res.locals.allowedOrganizations = authorities.reduce(
+      (previous, current) => {
+        const organizationId = current.match(/^organization:(\d{9}):admin$/);
+
+        return organizationId ? [...previous, organizationId[1]] : previous;
+      },
+      []
+    );
+  }
+
+  return hasSystemAdminRootPermission || hasOrganizationAdminPermissions;
 };
 
 export const enforceAuthority = (): RequestHandler => {
