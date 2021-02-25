8999c91a66a54ce889c9260d22b79195a88352df
NilsOveTen <github.nils.ove@tendenes.com>
2021-02-25 14:13:10 +0100

    chore: add more logs

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
@@ -6,6 +7,7 @@ import { commonErrorHandler } from './lib/common-error-handler';
 import { connectDb } from './db/db';
 import { createDataSourceRouter } from './data-source.router';
 import keycloak from './keycloak';
+import { streamLogger } from './logger';
 
 export async function createApp({
   connectionUris
@@ -23,6 +25,15 @@ export async function createApp({
   app.use('/api', createDataSourceRouter());
 
   app.use(commonErrorHandler);
+
+  app.use(
+    morgan('combined', {
+      skip: function (req: any) {
+        return req.url === '/ping' || req.url === '/ready';
+      },
+      stream: streamLogger
+    })
+  );
 
   await connectDb(connectionUris);
   return app;
