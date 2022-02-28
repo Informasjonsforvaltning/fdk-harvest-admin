0dce80875db4feab30bac022cddbb308b898bdfb
NilsOveTen <github.nils.ove@tendenes.com>
2022-02-28 09:56:30 +0100

    refactor: rm old nodejs code

--- src/db/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import logger from '../logger';
-
-export const connectDb = async (connectionUris: string): Promise<void> => {
-  await mongoose.connect(connectionUris, {});
-  mongoose.connection.on('error', logger.error);
-};
