30d8a4dbe15d89e2cd35f20e9465bbd44b8aee0e
NilsOveTen <github.nils.ove@tendenes.com>
2022-03-10 16:14:21 +0100

    refactor: implement enums & simple validation

--- model/dataSource.go
+++ model/dataSource.go
@@ -1,11 +1,19 @@
 package model
 
 type DataSource struct {
-	Id                string `json:"id" bson:"_id"`
-	DataSourceType    string `json:"dataSourceType" bson:"dataSourceType"`
-	DataType          string `json:"dataType" bson:"dataType"`
-	Url               string `json:"url" bson:"url"`
-	AcceptHeaderValue string `json:"acceptHeaderValue" bson:"acceptHeaderValue"`
-	PublisherId       string `json:"publisherId" bson:"publisherId"`
-	Description       string `json:"description" bson:"description"`
+	Id                string             `json:"id" bson:"_id"`
+	DataSourceType    DataSourceTypeEnum `json:"dataSourceType" bson:"dataSourceType"`
+	DataType          DataTypeEnum       `json:"dataType" bson:"dataType"`
+	Url               string             `json:"url" bson:"url"`
+	AcceptHeaderValue string             `json:"acceptHeaderValue" bson:"acceptHeaderValue"`
+	PublisherId       string             `json:"publisherId" bson:"publisherId"`
+	Description       string             `json:"description" bson:"description"`
+}
+
+func (dataSource DataSource) Validate() error {
+	err := dataSource.DataSourceType.Validate()
+	if err != nil {
+		return err
+	}
+	return dataSource.DataType.Validate()
 }
--- test/create_datasource_test.go
+++ test/create_datasource_test.go
@@ -47,3 +47,83 @@ func TestCreateDataSource(t *testing.T) {
 	toBeCreated.Id = actualResponse.Id
 	assert.Equal(t, toBeCreated, actualResponse)
 }
+
+func TestInvalidDataSourceType(t *testing.T) {
+	router := config.SetupRouter()
+
+	w := httptest.NewRecorder()
+	toBeCreated := model.DataSource{
+		DataSourceType:    "BAD-REQUEST",
+		DataType:          "concept",
+		Url:               "http://url.com",
+		AcceptHeaderValue: "text/turtle",
+		PublisherId:       "987654321",
+		Description:       "created source",
+	}
+	orgAdminAuth := OrgAdminAuth("987654321")
+	jwt := CreateMockJwt(time.Now().Add(time.Hour).Unix(), &orgAdminAuth, &TestValues.Audience)
+	body, _ := json.Marshal(toBeCreated)
+	req, _ := http.NewRequest("POST", "/organizations/987654321/datasources", bytes.NewReader(body))
+	req.Header.Set("Authorization", *jwt)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestInvalidDataType(t *testing.T) {
+	router := config.SetupRouter()
+
+	w := httptest.NewRecorder()
+	toBeCreated := model.DataSource{
+		DataSourceType:    "SKOS-AP-NO",
+		DataType:          "invalid",
+		Url:               "http://url.com",
+		AcceptHeaderValue: "text/turtle",
+		PublisherId:       "987654321",
+		Description:       "created source",
+	}
+	orgAdminAuth := OrgAdminAuth("987654321")
+	jwt := CreateMockJwt(time.Now().Add(time.Hour).Unix(), &orgAdminAuth, &TestValues.Audience)
+	body, _ := json.Marshal(toBeCreated)
+	req, _ := http.NewRequest("POST", "/organizations/987654321/datasources", bytes.NewReader(body))
+	req.Header.Set("Authorization", *jwt)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestInvalidJSON(t *testing.T) {
+	router := config.SetupRouter()
+
+	w := httptest.NewRecorder()
+	orgAdminAuth := OrgAdminAuth("987654321")
+	jwt := CreateMockJwt(time.Now().Add(time.Hour).Unix(), &orgAdminAuth, &TestValues.Audience)
+	body := []byte("{''}")
+	req, _ := http.NewRequest("POST", "/organizations/987654321/datasources", bytes.NewReader(body))
+	req.Header.Set("Authorization", *jwt)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestWrongOrganization(t *testing.T) {
+	router := config.SetupRouter()
+
+	w := httptest.NewRecorder()
+	toBeCreated := model.DataSource{
+		DataSourceType:    "SKOS-AP-NO",
+		DataType:          "concept",
+		Url:               "http://url.com",
+		AcceptHeaderValue: "text/turtle",
+		PublisherId:       "123456789",
+		Description:       "created source",
+	}
+	orgAdminAuth := OrgAdminAuth("987654321")
+	jwt := CreateMockJwt(time.Now().Add(time.Hour).Unix(), &orgAdminAuth, &TestValues.Audience)
+	body, _ := json.Marshal(toBeCreated)
+	req, _ := http.NewRequest("POST", "/organizations/987654321/datasources", bytes.NewReader(body))
+	req.Header.Set("Authorization", *jwt)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
