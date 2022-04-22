c4697cec5266991e0e14840bb0190484299faa7a
NilsOveTen <github.nils.ove@tendenes.com>
2022-04-22 09:48:58 +0200

    refactor: handler function declarations

--- handlers/readyHandler.go
+++ handlers/readyHandler.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var ReadyHandler = func() func(c *gin.Context) {
+func ReadyHandler() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		c.Status(http.StatusOK)
 	}
