e8467033bbd620e893db85af012d763dc4b79319
Kurt Stian Fagerbakk <43232700+KurtStian@users.noreply.github.com>
2023-06-19 14:12:33 +0200

    feat: add DatasourceType ModellDCAT-AP-NO

--- model/dataSourceTypeEnum.go
+++ model/dataSourceTypeEnum.go
@@ -9,11 +9,12 @@ const (
 	DcatApNo DataSourceTypeEnum = "DCAT-AP-NO"
 	CpsvApNo DataSourceTypeEnum = "CPSV-AP-NO"
 	Tbx      DataSourceTypeEnum = "TBX"
+	ModellDcatApNo DataSourceTypeEnum = "ModellDCAT-AP-NO"
 )
 
 func (dataSourceType DataSourceTypeEnum) Validate() error {
 	switch dataSourceType {
-	case SkosApNo, DcatApNo, CpsvApNo, Tbx:
+	case SkosApNo, DcatApNo, CpsvApNo, Tbx, ModellDcatApNo:
 		return nil
 	}
 	return fmt.Errorf("%s is not a valid data source type", dataSourceType)
